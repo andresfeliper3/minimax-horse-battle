ccda8e939845fa7e17e92985fb6c848b71cc42ab
andresfeliper3 <andresfeliper.2001@gmail.com>
2023-01-08 13:13:31 -0500

    initial version of grid

---
+++ app.js
@@ -0,0 +1,32 @@
+const boxesPerRow = 8;
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(220);
+
+    drawGrid();
+}
+
+
+function drawGrid() {
+    let column = 0;
+    let row = 0;
+    for (let x = 0; x < width; x += width / boxesPerRow) {
+        for (let y = 0; y < height; y += height / boxesPerRow) {
+            stroke(0);
+            strokeWeight(1);
+            line(x, 0, x, height);
+            line(0, y, width, y);
+
+            column++;
+        }
+        row++;
+    }
+}
+
+//THIS MAKES THE CODE WORK
+window.setup = setup;
+window.draw = draw;
