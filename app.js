54acf67cf5a774bf61b709e25bc13fd41d050455
David Hernández <josedavid_0727@outlook.com>
2023-01-08 15:53:43 -0500

    bonus img

--- app.js
+++ app.js
@@ -2,82 +2,77 @@ const boxesPerRow = 8;
 
 let black;
 let white;
+let bonus;
 let boxWidth;
 let boxHeight;
 
 let lastMove = {};
 
 function preload() {
-    black = loadImage("src/images/black.png");
-    white = loadImage("src/images/white.png");
+  black = loadImage("src/images/black.png");
+  white = loadImage("src/images/white.png");
+  bonus = loadImage("src/images/bonus.png");
 }
 
-
 function setup() {
-    preload();
-    createCanvas(500, 500);
-    background(220);
+  preload();
+  createCanvas(500, 500);
+  background(220);
 }
 
 function draw() {
-    boxWidth = width / boxesPerRow;
-    boxHeight = height / boxesPerRow;
+  boxWidth = width / boxesPerRow;
+  boxHeight = height / boxesPerRow;
 
-    drawGrid();
+  drawGrid();
 
-    dominateBox();
-
+  dominateBox();
 }
 
 function drawGrid() {
-    let column = 0;
-    let row = 0;
-    for (let x = 0; x < width; x += boxWidth) {
-        for (let y = 0; y < height; y += boxHeight) {
-            stroke(0);
-            strokeWeight(1);
-            line(x, 0, x, height); //vertical
-            line(0, y, width, y); //horizontal
-            column++;
-        }
-        row++;
+  let column = 0;
+  let row = 0;
+  for (let x = 0; x < width; x += boxWidth) {
+    for (let y = 0; y < height; y += boxHeight) {
+      stroke(0);
+      strokeWeight(1);
+      line(x, 0, x, height); //vertical
+      line(0, y, width, y); //horizontal
+      column++;
     }
+    row++;
+  }
 }
 
-
 function dominateBox() {
-    if (mouseIsPressed) {
-        eraseHorse("green");
-        calculateAndPaintBox("green");
-    }
+  if (mouseIsPressed) {
+    eraseHorse("green");
+    calculateAndPaintBox("green");
+  }
 }
 
 function eraseHorse(color) {
-    fill(color);
-    rect(lastMove.x, lastMove.y, boxWidth, boxHeight)
+  fill(color);
+  rect(lastMove.x, lastMove.y, boxWidth, boxHeight);
 }
 function calculateAndPaintBox(color) {
-    let boxIndex = calculateBoxIndex();
-    let position = { x: boxIndex.row * boxWidth, y: boxIndex.col * boxHeight }
-    paintBox(position, color)
+  let boxIndex = calculateBoxIndex();
+  let position = { x: boxIndex.row * boxWidth, y: boxIndex.col * boxHeight };
+  paintBox(position, color);
 }
 
 function calculateBoxIndex() {
-    let boxRow = Math.floor(mouseX / boxWidth);
-    let boxCol = Math.floor(mouseY / boxHeight);
-    return { row: boxRow, col: boxCol }
+  let boxRow = Math.floor(mouseX / boxWidth);
+  let boxCol = Math.floor(mouseY / boxHeight);
+  return { row: boxRow, col: boxCol };
 }
 
 function paintBox(position, color) {
-    fill(color);
-    rect(position.x, position.y, boxWidth, boxHeight)
-    image(white, position.x, position.y);
-    lastMove = { x: position.x, y: position.y };
+  fill(color);
+  rect(position.x, position.y, boxWidth, boxHeight);
+  image(white, position.x, position.y);
+  lastMove = { x: position.x, y: position.y };
 }
-
-
-
-
 
 //THIS MAKES THE CODE WORK
 window.setup = setup;
