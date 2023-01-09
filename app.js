d2874cb3ecd0e53ed09c99f84cf39784ce149770
David Hernández <josedavid_0727@outlook.com>
2023-01-09 13:57:50 -0500

    mouseClicked issue

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const boxesPerRow = 8;
 
 let black;
 let white;
-let bonus;
+let bonusImage;
 let boxWidth;
 let boxHeight;
 
+let initialState;
+
 let lastMove = {};
+
+const bonusIndex = [
+  { row: 0, col: 1 },
+  { row: 5, col: 2 },
+  { row: 2, col: 2 },
+];
 
 function preload() {
   black = loadImage("src/images/black.png");
   white = loadImage("src/images/white.png");
-  bonus = loadImage("src/images/bonus.png");
+  bonusImage = loadImage("src/images/bonus.png");
 }
 
 function setup() {
@@ -23,12 +31,11 @@ function setup() {
 function draw() {
   boxWidth = width / boxesPerRow;
   boxHeight = height / boxesPerRow;
-
   drawGrid();
 
   dominateBox();
+  paintbonus();
 }
-
 function drawGrid() {
   let column = 0;
   let row = 0;
@@ -45,10 +52,21 @@ function drawGrid() {
 }
 
 function dominateBox() {
-  if (mouseIsPressed) {
+  if (mouseIsPressed && checkHorseMovement()) {
+    //calculate pos
+    //check
     eraseHorse("green");
     calculateAndPaintBox("green");
   }
+}
+
+function checkHorseMovement() {
+  let allowMove = true;
+  let boxIndex = calculateBoxIndex(lastMove.x, lastMove.y);
+  let position = { x: boxIndex.row * boxWidth, y: boxIndex.col * boxHeight };
+  console.log("lastMove: ", boxIndex);
+
+  return allowMove;
 }
 
 function eraseHorse(color) {
@@ -56,14 +74,14 @@ function eraseHorse(color) {
   rect(lastMove.x, lastMove.y, boxWidth, boxHeight);
 }
 function calculateAndPaintBox(color) {
-  let boxIndex = calculateBoxIndex();
+  let boxIndex = calculateBoxIndex(mouseX, mouseY);
   let position = { x: boxIndex.row * boxWidth, y: boxIndex.col * boxHeight };
   paintBox(position, color);
 }
 
-function calculateBoxIndex() {
-  let boxRow = Math.floor(mouseX / boxWidth);
-  let boxCol = Math.floor(mouseY / boxHeight);
+function calculateBoxIndex(x, y) {
+  let boxRow = Math.floor(x / boxWidth);
+  let boxCol = Math.floor(y / boxHeight);
   return { row: boxRow, col: boxCol };
 }
 
@@ -72,6 +90,12 @@ function paintBox(position, color) {
   rect(position.x, position.y, boxWidth, boxHeight);
   image(white, position.x, position.y);
   lastMove = { x: position.x, y: position.y };
+}
+
+function paintbonus() {
+  bonusIndex.forEach((bonus) =>
+    image(bonusImage, bonus.row * boxWidth + 2, bonus.col * boxHeight + 2)
+  );
 }
 
 //THIS MAKES THE CODE WORK
